985ec2a619e303d077b05161591a4d513dd4aefa
DEEPAK820574 <97129610+DEEPAK820574@users.noreply.github.com>
2024-10-02 14:09:56 +0530

    Update Price.jsx

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -4,10 +4,10 @@ import PriceDetail from "../Components/PriceComponent/PriceDetail";
 
 const Price = () => {
   return (
-    <div
+     <div
       class=" absolute top-[2086px] w-[1440px] h-[798px] overflow-hidden "
       style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/homebg.png)`,
+        backgroundImage: `url("/assets/homebg.png")`,
         backgroundSize: "1440px 798px",
         backgroundPosition: "center",
       }}
@@ -15,7 +15,7 @@ const Price = () => {
       <div
         class=" absolute  w-[1440px] h-[798px] z-20  "
         style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL}/assets/pricewhiteboard.png)`,
+          backgroundImage: `url("/assets/pricewhiteboard.png")`,
           backgroundSize: "1440px 798px",
           backgroundPosition: "center",
         }}
